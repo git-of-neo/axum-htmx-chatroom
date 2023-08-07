7eeb1dae5e3e1a630f13f67462d82337cbfe60ba
git-of-neo <nshmail55@gmail.com>
2023-08-07 21:05:07 +0800

    [add] Minimal askama_axum response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::routing;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+#[allow(dead_code)]
+struct IndexTemplate<'a> {
+    name: &'a str,
+}
+
+async fn index() -> IndexTemplate<'static> {
+    IndexTemplate { name: "John Doe" }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/", routing::get(index));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
