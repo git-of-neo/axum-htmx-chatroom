45b2771817c88efd72ded8df9e6450885accfd6a
git-of-neo <nshmail55@gmail.com>
2023-08-11 00:21:58 +0800

    [add] ability to send messages to server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
+use std::sync::{Arc, Mutex};
+
 use askama::Template;
-use axum::routing;
+use axum::{extract::State, http::StatusCode, routing, Form};
 use rand::Rng;
+use serde::Deserialize;
+
+struct AppState {
+    msgs: Mutex<Vec<String>>,
+}
 
 #[tokio::main]
 async fn main() {
+    let state = Arc::new(AppState {
+        msgs: Mutex::new(Vec::from(
+            [
+                "Hey there! How's your day going?",
+                "Did you catch that new movie everyone's talking about?",
+                "Pizza or tacos? It's the ultimate food debate!",
+                "Just wanted to say hi and spread some positivity your way!",
+                "I'm counting down the days until the weekend. Anyone else?",
+                "Have you ever tried bungee jumping? It's on my bucket list!",
+                "Guess what? I adopted a puppy and my heart's melting.",
+                "Quick poll: Cats or dogs? Let the battle of cuteness begin!",
+                "Netflix recommendations, anyone? I've watched everything on my list.",
+                "If you could travel anywhere right now, where would you go?",
+            ]
+            .map(|s| s.to_owned()),
+        )),
+    });
+
     let app = axum::Router::new()
         .route("/", routing::get(index))
-        .route("/chat", routing::get(chat));
+        .route("/chat", routing::get(chat))
+        .route("/chat", routing::post(create_chat))
+        .with_state(state);
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -26,24 +53,29 @@ async fn index() -> IndexTemplate<'static> {
 
 #[derive(Template)]
 #[template(path = "chat.html")]
-struct ChatTemplate<'a> {
-    msg: &'a str,
+struct ChatTemplate {
+    msg: String,
 }
 
-async fn chat() -> ChatTemplate<'static> {
-    let msgs = [
-        "Hey there! How's your day going?",
-        "Did you catch that new movie everyone's talking about?",
-        "Pizza or tacos? It's the ultimate food debate!",
-        "Just wanted to say hi and spread some positivity your way!",
-        "I'm counting down the days until the weekend. Anyone else?",
-        "Have you ever tried bungee jumping? It's on my bucket list!",
-        "Guess what? I adopted a puppy and my heart's melting.",
-        "Quick poll: Cats or dogs? Let the battle of cuteness begin!",
-        "Netflix recommendations, anyone? I've watched everything on my list.",
-        "If you could travel anywhere right now, where would you go?",
-    ];
+async fn chat<'a>(State(state): State<Arc<AppState>>) -> ChatTemplate {
+    let msgs = state.msgs.lock().unwrap();
+    let msg = &msgs[rand::thread_rng().gen_range(0..msgs.len())];
+
     ChatTemplate {
-        msg: &msgs[rand::thread_rng().gen_range(0..msgs.len())],
+        msg: msg.to_string(),
     }
+}
+
+#[derive(Deserialize)]
+struct NewChat {
+    message: String,
+}
+
+async fn create_chat(
+    State(state): State<Arc<AppState>>,
+    Form(chat): Form<NewChat>,
+) -> (StatusCode, ()) {
+    let mut msgs = state.msgs.lock().unwrap();
+    msgs.push(chat.message.to_owned());
+    (StatusCode::CREATED, ())
 }
